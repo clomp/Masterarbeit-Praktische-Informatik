338d7496202bdf494d184c8530f5e92987ea5bd2
Christian Lomp <christian.lomp@studium.fernuni-hagen.de>
2023-01-17 13:51:56 +0100

    11
    11

--- code/GP-EnKF-master/gpenkf/gp_util/squared_exponential.py
+++ code/GP-EnKF-master/gpenkf/gp_util/squared_exponential.py
@@ -3,6 +3,8 @@ from scipy.spatial.distance import cdist
 
 from gpenkf.gp_util.covariance_function import CovarianceFunction
 
+#sqeuclidean to seuclidean   
+
 
 class SquaredExponential(CovarianceFunction):
 
@@ -17,15 +19,15 @@ class SquaredExponential(CovarianceFunction):
         return cov_func
 
     def eval_cov_matrix(self, x1, x2):
-        return self.variance * np.exp(-cdist(x1, x2, 'sqeuclidean', V=2 * self.lengthscale ** 2))
+        return self.variance * np.exp(-cdist(x1, x2, 'seuclidean', V=2 * self.lengthscale ** 2))
 
     def predict(self, x_new, x_g, noise_variance, g):
         # TODO estimate lengthscale for each dimension
-        K_new_g = self.variance * np.exp(-cdist(x_new, x_g, 'sqeuclidean', V=2 * self.lengthscale ** 2))
+        K_new_g = self.variance * np.exp(-cdist(x_new, x_g, 'seuclidean', V=2 * self.lengthscale ** 2))
 
-        K_g_g = self.variance * np.exp(-cdist(x_g, x_g, 'sqeuclidean', V=2 * self.lengthscale ** 2))
+        K_g_g = self.variance * np.exp(-cdist(x_g, x_g, 'seuclidean', V=2 * self.lengthscale ** 2))
 
-        K_new_new = self.variance * np.exp(-cdist(x_new, x_new, 'sqeuclidean', V=2 * self.lengthscale ** 2))
+        K_new_new = self.variance * np.exp(-cdist(x_new, x_new, 'seuclidean', V=2 * self.lengthscale ** 2))
 
         pred_mean = np.dot(K_new_g, np.linalg.solve(K_g_g + noise_variance * np.eye(x_g.shape[0]), g))
         pred_cov = K_new_new - np.dot(K_new_g,
@@ -33,7 +35,12 @@ class SquaredExponential(CovarianceFunction):
         return pred_mean, pred_cov
 
     def log_likelihood(self, y, x_g, noise_variance):
-        K_g_g = self.variance * np.exp(-cdist(x_g, x_g, 'sqeuclidean', V=2 * self.lengthscale ** 2))
+        if(type(self.lengthscale) != np.ndarray):
+            a = np.array([self.lengthscale])
+        else:
+            a=self.lengthscale
+        
+        K_g_g = self.variance * np.exp(-cdist(x_g, x_g, 'seuclidean', V=2 * a ** 2))
 
         return -0.5 * (np.dot(y.T, np.linalg.solve(K_g_g + noise_variance * np.eye(x_g.shape[0]), y))
                        + np.log(np.linalg.det(K_g_g + noise_variance * np.eye(x_g.shape[0])))
